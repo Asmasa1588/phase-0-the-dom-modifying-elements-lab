f08c3e2ef95c13d95785b16884c35dc0240e5153
Asma <as1588ma@gmail.com>
2022-04-17 09:09:08 -0400

    commit tree a2ea2e9530e6c847faafed9284b912ca06697524

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your code here!
+const mainElement = document.getElementById("main");
+mainElement.remove();
+const newHeader = document.createElement("h1");
+newHeader.setAttribute("id", "victory");
+newHeader.innerHTML = "ASMA SADIQ is the champion ";
